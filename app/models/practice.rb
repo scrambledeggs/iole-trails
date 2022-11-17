9cf4d3bff636b4e0204ed401cf22eb495a1d32d4
iole-geil <iolegeil@booky.ph>
2022-11-17 13:40:56 +0800

    Added Practices migration

---
+++ app/models/practice.rb
@@ -0,0 +1,4 @@
+class Practice < ApplicationRecord
+  belongs_to :person
+  belongs_to :trail
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_17_051423) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_17_052208) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,16 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_17_051423) do
     t.float "weight"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "practices", force: :cascade do |t|
+    t.integer "status", default: 0
+    t.bigint "person_id", null: false
+    t.bigint "trail_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["person_id"], name: "index_practices_on_person_id"
+    t.index ["trail_id"], name: "index_practices_on_trail_id"
   end
 
   create_table "trails", force: :cascade do |t|
@@ -34,4 +44,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_17_051423) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "practices", "people"
+  add_foreign_key "practices", "trails"
 end
