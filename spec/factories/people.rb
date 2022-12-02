40a17d6f9d8648654991a3a0d1b708066e098db4
iole-geil <iolegeil@booky.ph>
2022-12-02 12:47:02 +0800

    Person model validation tests

---
+++ spec/factories/people.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :person do
+    name { "MyString" }
+    age { 1 }
+    weight { 1.5 }
+    body_build { 1 }
+  end
+end
---
+++ spec/models/person_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe Person, type: :model do
+  it 'is valid with valid attributes' do
+    person1 = create(:person)
+    expect(person1).to be_valid
+  end
+
+  it 'is not valid without a name' do
+    person2 = build(:person, name: nil)
+    expect(person2).to_not be_valid
+  end
+
+  it 'is not valid without an age' do
+    person2 = build(:person, age: nil)
+    expect(person2).to_not be_valid
+  end
+
+  it 'is not valid without a weight' do
+    person2 = build(:person, weight: nil)
+    expect(person2).to_not be_valid
+  end
+
+  it 'is not valid without a body build' do
+    person2 = build(:person, body_build: nil)
+    expect(person2).to_not be_valid
+  end
+end
