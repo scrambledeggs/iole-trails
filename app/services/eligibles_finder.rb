8fe89f743c018034e853958ba3001d94a897c742
iole <iolegeil@booky.ph>
2023-01-06 10:04:32 +0800

    Updated eligibles_finder based on db update

--- app/services/eligibles_finder.rb
+++ app/services/eligibles_finder.rb
@@ -4,9 +4,13 @@ class EligiblesFinder < ApplicationService
   end
 
   def call
-    # TODO: update using updated db schema
+    @trail.age_minimum ||= 1
+    @trail.age_maximum ||= 100
+    date_maximum = Date.today - @trail.age_minimum.years
+    date_minimum = Date.today - @trail.age_maximum.years - 1.year
+
     Person.where(
-      age: [@trail.age_minimum..@trail.age_maximum],
+      birthdate: [date_minimum..date_maximum],
       weight: [@trail.weight_minimum..@trail.weight_maximum],
       body_build: @trail.body_build || Person.body_builds.keys
     )
