2b2bc2186dca349f6e56830bece249042490ddf8
iole <iolegeil@booky.ph>
2023-01-23 16:07:19 +0800

    Added helper for coloring status

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def status_tag(status)
+    status_color = case status
+                   when 'ONGOING', 'STARTED'
+                     'ongoing'
+                   when 'REGISTERED', 'NEW'
+                     'new'
+                   when 'FINISHED'
+                     'finished'
+                   when 'UNFINISHED', 'DROPPED'
+                     'unfinished'
+                   end
+    "<mark class=#{status_color}>#{status}</mark>".html_safe
+  end
 end
